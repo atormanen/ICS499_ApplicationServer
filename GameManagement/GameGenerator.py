2e629ac416e2a782b3aaa16b37dc2782e801a23a
Asher <TormanenAsher@gmailc.om>
2020-03-28 13:36:14 -0500

    test

--- GameManagement/GameGenerator.py
+++ GameManagement/GameGenerator.py
@@ -56,6 +56,7 @@ class GameGenerator:
         reqItem.createGameRespNotAccepted(playerOne, gameToken)
 
     def waitForPlayer(self, gameToken):
+        print(self.gameCollection.getGame)
         while(self.gameCollection.getGame == False):
             time.sleep(2)
 

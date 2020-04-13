cd4ce3b299f917d407e07abf144c09b1a97cb24e
Tormanen <ATormanen@datarecognitioncorp.com>
2020-04-13 10:36:42 -0500

    test

--- GameManagement/Game.py
+++ GameManagement/Game.py
@@ -27,7 +27,7 @@ class Game:
 
     def listen(self, socket):
         while(True):
-            #msg = socket.recv(1024)
+            #msg = socket.recv(1024) test
             self.listener.processRequest(socket,(self.player_one_ip,self.player_two_port))
 
 

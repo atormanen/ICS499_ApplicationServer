5335581d8140631b1dfac2b5cb9df6423751f747
Asher <TormanenAsher@gmailc.om>
2020-04-18 14:37:28 -0500

    syntax indentation error

--- DataManagement/MessageItem.py
+++ DataManagement/MessageItem.py
@@ -1,13 +1,14 @@
 import json
-#Message item is a wrapper class to hold the data of each reqeust.
-#It holds the json object that was sent to the server as well as
-#the socket
+# Message item is a wrapper class to hold the data of each reqeust.
+# It holds the json object that was sent to the server as well as
+# the socket
 
-## TODO: Create subclasses for each message type.
-#Too much happening here
+# TODO: Create subclasses for each message type.
+# Too much happening here
+
 
 class MessageItem:
-    def __init__(self,connectionSocket, addr, parsedData):
+    def __init__(self, connectionSocket, addr, parsedData):
         self.connectionSocket = connectionSocket
         self.ipAddress = addr[0]
         self.port = addr[1]
@@ -16,9 +17,9 @@ class MessageItem:
 
     def createGameRespNotAccepted(self, playerOne, gameToken):
         response = {
-                    "requestType":"CreateGame",
-                    "player_one":"",
-                    "game_token":""
+            "requestType": "CreateGame",
+            "player_one": "",
+            "game_token": ""
         }
         response["player_one"] = playerOne
         response["game_token"] = gameToken
@@ -26,10 +27,10 @@ class MessageItem:
 
     def acceptGame(self, playerOne, playerTwo, gameToken):
         response = {
-                    "requestType":"CreateGame",
-                    "player_one":"",
-                    "player_two":"",
-                    "game_token":""
+            "requestType": "CreateGame",
+            "player_one": "",
+            "player_two": "",
+            "game_token": ""
         }
         response["player_one"] = playerOne
         response["player_two"] = playerTwo
@@ -38,9 +39,9 @@ class MessageItem:
 
     def checkForGameResponse(self, playerOne, gameToken):
         response = {
-                    "requestType":"CreateGame",
-                    "username":"",
-                    "game_token":""
+            "requestType": "CreateGame",
+            "username": "",
+            "game_token": ""
         }
         response["username"] = playerOne
         response["game_token"] = gameToken
@@ -48,16 +49,16 @@ class MessageItem:
 
     def createRandomGameResp(self, game):
         response = {
-                    "requestType":"RequestGame",
-                    "status":"success",
-                    "game_token":"",
-                    "player_one":"",
-                    "player_two":"",
-                    "player_one_color":"",
-                    "player_two_color":"",
-                    "player_one_ip":"",
-                    "player_one_port":"",
-                    "player_two_ip":""
+            "requestType": "RequestGame",
+            "status": "success",
+            "game_token": "",
+            "player_one": "",
+            "player_two": "",
+            "player_one_color": "",
+            "player_two_color": "",
+            "player_one_ip": "",
+            "player_one_port": "",
+            "player_two_ip": ""
         }
         response["game_token"] = game.gameToken
         response["player_one"] = game.player_one
@@ -69,16 +70,16 @@ class MessageItem:
         response["player_two_ip"] = game.player_two_ip
         self.responseObj = json.dumps(response)
 
-        def createRandomGameRespFailure(self, username, status, reason):
-            response = {
-                        "requestType":"RequestGame",
-                        "player_one":"",
-                        "status":"",
-                        "reason":""
-            }
-            response["game_token"] = game.gameToken
-            response["player_one"] = game.player_one
-            response["status"] = status
-            response["reason"] = reason
+    def createRandomGameRespFailure(self, username, status, reason):
+        response = {
+            "requestType": "RequestGame",
+            "player_one": "",
+            "status": "",
+            "reason": ""
+        }
+        response["game_token"] = game.gameToken
+        response["player_one"] = game.player_one
+        response["status"] = status
+        response["reason"] = reason
 
-            self.responseObj = json.dumps(response)
+        self.responseObj = json.dumps(response)

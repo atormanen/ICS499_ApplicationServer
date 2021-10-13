5818c88c8953973791faa05d2acfb70501d39380
Asher <TormanenAsher@gmail.com>
2021-10-12 21:13:21 -0500

    update git pull

--- git_pull.sh
+++ git_pull.sh
@@ -4,8 +4,10 @@ git stash
 git fetch
 git pull
 mkdir -p ./logs
+chown -R jar_user:jar_user *
+chmod 0554 -R *
+chmod 0664 ./logs/app_server.log
+chmod 0440 ./params.json
 touch ./logs/app_server.log
-chmod +x -R *
-chmod +w ./logs/app_server.log
 systemctl restart jar_app.service
 tail -n 100 -f ./logs/app_server.log
